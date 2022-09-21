db5e82c9090077c174a7bf014ea70ab2b774d49a
Juilzze <110187961+Juilzze@users.noreply.github.com>
2022-09-21 13:25:04 +0300

    newsql file

--- bansql.sql
+++ bansql.sql
@@ -19,7 +19,7 @@ USE `bans_2022`;
 
 -- Dumping structure for table bans_2022.bans
 CREATE TABLE IF NOT EXISTS `bans` (
-  `banid` int(11) NOT NULL,
+  `banid` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(30) DEFAULT NULL,
   `reason` varchar(100) DEFAULT NULL,
   `date` varchar(100) DEFAULT NULL,
@@ -27,11 +27,16 @@ CREATE TABLE IF NOT EXISTS `bans` (
   PRIMARY KEY (`banid`) USING BTREE,
   KEY `username` (`username`),
   CONSTRAINT `bans_ibfk_1` FOREIGN KEY (`username`) REFERENCES `users` (`username`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4;
 
--- Dumping data for table bans_2022.bans: ~0 rows (approximately)
+-- Dumping data for table bans_2022.bans: ~4 rows (approximately)
 DELETE FROM `bans`;
 /*!40000 ALTER TABLE `bans` DISABLE KEYS */;
+INSERT INTO `bans` (`banid`, `username`, `reason`, `date`, `enddate`) VALUES
+	(1, '123v3v1', 'test', '1/1/2000', '1/2/2000'),
+	(2, '123v3v1', 'test', '1/1/2000', '1/2/2000'),
+	(3, '123v3v1', 'test', '1/1/2000', '1/2/2000'),
+	(4, 'Juuso', 'Testi ban', '2022-09-21', '2022-10-01');
 /*!40000 ALTER TABLE `bans` ENABLE KEYS */;
 
 -- Dumping structure for table bans_2022.users
@@ -42,11 +47,27 @@ CREATE TABLE IF NOT EXISTS `users` (
   PRIMARY KEY (`username`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- Dumping data for table bans_2022.users: ~1 rows (approximately)
+-- Dumping data for table bans_2022.users: ~17 rows (approximately)
 DELETE FROM `users`;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
 INSERT INTO `users` (`username`, `pass`, `admin`) VALUES
-	('Juuso', 'moi123', 1);
+	('', '', 0),
+	('1', '0', 0),
+	('123', '55', 1),
+	('1231', '123', 1),
+	('12399', '55', 1),
+	('123v3v1', '1v233v12', 1),
+	('itsesaatana', 'lol', 1),
+	('ja22aha22s2', 'ha2a2', 0),
+	('ja22aha22s2jaaaaaaaa', 'ha2a2', 0),
+	('ja22aha2s2', 'ha2a2', 0),
+	('jaaha2s2', 'ha2a2', 0),
+	('jaahas', 'haa', 0),
+	('jaahas2', 'haa2', 0),
+	('Juuso', 'moi123', 1),
+	('KaHipu', 'KaHipu', 0),
+	('superidol', 'kana', 1),
+	('testiii123', '123', 1);
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 
 -- Dumping structure for table bans_2022.warns
@@ -60,9 +81,11 @@ CREATE TABLE IF NOT EXISTS `warns` (
   CONSTRAINT `warns_ibfk_1` FOREIGN KEY (`username`) REFERENCES `users` (`username`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- Dumping data for table bans_2022.warns: ~0 rows (approximately)
+-- Dumping data for table bans_2022.warns: ~1 rows (approximately)
 DELETE FROM `warns`;
 /*!40000 ALTER TABLE `warns` DISABLE KEYS */;
+INSERT INTO `warns` (`warnid`, `username`, `reason`, `date`) VALUES
+	(1, 'testiii123', 'Testaus', '21/09/2022');
 /*!40000 ALTER TABLE `warns` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
